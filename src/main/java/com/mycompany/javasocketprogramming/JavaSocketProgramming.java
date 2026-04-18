62cd28ab36947188694d41737fa2d53a573ea5e2
Jonathan102-hub <jonathan.hendri@mhs.unj.ac.id>
2026-04-18 21:25:53 +0700

    Start

---
+++ src/main/java/com/mycompany/javasocketprogramming/JavaSocketProgramming.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javasocketprogramming;
+
+/**
+ *
+ * @author User
+ */
+public class JavaSocketProgramming {
+
+    public static void main(String[] args) {
+        System.out.println("Ini saatnya");
+    }
+}
